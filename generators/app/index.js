c0c5270d4b15141d65bc84b8141d46d2b9daf562
DezkaReid <elmaildeldezkareid@gmail.com>
2018-06-20 16:40:06 -0500

    Added method to install dependencies

--- generators/app/index.js
+++ generators/app/index.js
@@ -36,6 +36,26 @@ module.exports = class extends Generator {
     this.fs.copy(this.templatePath('webpack/'), this.destinationPath('./'))
   }
 
+  install () {
+    const productionDependencies = ['react', 'react-dom', 'react-router-dom']
+    const developmentDependencies = [
+      'babel-core',
+      'babel-loader',
+      'babel-plugin-transform-object-rest-spread',
+      'babel-preset-env',
+      'babel-preset-react',
+      'css-loader',
+      'style-loader',
+      'webpack',
+      'webpack-cli',
+      'webpack-dev-server',
+      'webpack-merge',
+      'html-webpack-plugin',
+      'webpack-bundle-analyzer'
+    ]
+    this.yarnInstall(productionDependencies)
+      .then(() => this.yarnInstall(developmentDependencies, { 'dev': true }))
+  }
   end () {
     this.log('Scaffolding finished')
   }
