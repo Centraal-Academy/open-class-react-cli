1f45aa81620235eaa1641cd96fd5483e0c488f14
DezkaReid <elmaildeldezkareid@gmail.com>
2018-06-20 13:02:26 -0500

    life cycle

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,6 +1,30 @@
 const Generator = require('yeoman-generator')
 module.exports = class extends Generator {
-  hello () {
-    console.log('Hello World')
+  constructor (args, opts) {
+    super(args, opts)
+    this.log('constructor')
+  }
+  initializing () {
+    this.log('starting own generator')
+  }
+
+  configuring () {
+    this.log('configuring own generator')
+  }
+
+  prompting () {
+    this.log('propmting generator')
+  }
+
+  writing () {
+    this.log('writing files')
+  }
+
+  install () {
+    this.log('install dependencies')
+  }
+
+  end () {
+    this.log('This is over')
   }
 }
